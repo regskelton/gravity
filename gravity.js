e15198c8cde404c91052666e224af24ad51502ff
Lorraine Skelton <lorraine@Korma-iMac-Study.local>
2018-12-30 22:56:03 +0000

    fixed unstable orbits added first css added named things display info about things

--- gravity.js
+++ gravity.js
@@ -1,7 +1,8 @@
 var universe = (function () {
     var nDims, scale, bigG, things = [];
 
-    var Thing = function (initialPosition, initialVelocity, mass, size, color) {
+    var Thing = function (name, initialPosition, initialVelocity, mass, size, color) {
+        this.name = name;
         this.position = initialPosition;
         this.velocity = initialVelocity;
         this.mass = mass;
@@ -65,8 +66,8 @@ var universe = (function () {
             bigG = bigGparam;
         },
 
-        createSomething: function (position, velocity, mass, size, color) {
-            things.push(new Thing(position, velocity, mass, size, color));
+        createSomething: function (name, position, velocity, mass, size, color) {
+            things.push(new Thing(name, position, velocity, mass, size, color));
         },
 
         moveAll: function () {
@@ -187,134 +188,274 @@ var uiController = (function () {
     var scale;
 
     var DOM = {
-        dimensions: document.getElementById('dimensions'),
-        momentum: document.getElementById('momentum'),
-        kinetic: document.getElementById('kinetic')
+        dimensions: document.querySelector('.info__dimensions--value'),
+        momentum: document.querySelector('.info__momentum--value'),
+        kinetic: document.querySelector('.info__kinetic--value'),
+        potential: document.querySelector('.info__potential--value'),
+        total: document.querySelector('.info__total--value'),
+        item_list: document.querySelector('.item__list'),
     };
-
-    function draw(thing) {
-        drawAt(thing.position, thing.velocity, thing.acceleration, thing.size, thing.color);
-    }
 
     var velocityScale = Math.pow(10, -3);
     var accelerationScale = 2.5 * Math.pow(10, 4);
 
-    function drawAt(position, velocity, acceleration, size, color) {
-        var positionScale = {
-            width: canvas.width / scale,
-            height: canvas.height / scale
+
+    function formatLargeNumber(num, dp) {
+        var log= Math.log10(num);
+
+        var exp = Math.floor(log);
+        var mantissa = Math.pow( 10, log - exp).toFixed(dp);
+
+        //var numText = "" + num;
+
+        //var mantissa = numText[0] + "." + numText.slice(2, 2 + dp);
+
+        //console.log( num, dp, mantissa, ":", exp);
+
+        return mantissa + ":" + exp;
+    }
+
+
+    var toPolar = function (inputVector) {
+        var sumOfSquares = 0;
+
+        for (d = 0; d < inputVector.length; d++) {
+            sumOfSquares += inputVector[d] * inputVector[d];
         }
 
-        var centre = {
-            x: position[0] * positionScale.width,
-            y: position[1] * positionScale.height
-        }
-
-        var velocityVector = {
-            x: centre.x + velocity[0] * velocityScale,
-            y: centre.y + velocity[1] * velocityScale
-        }
-
-        var accelerationVector = {
-            x: centre.x + acceleration[0] * accelerationScale,
-            y: centre.y + acceleration[1] * accelerationScale
-        }
-
-
-        //draw velocity and the acceleration vectors
-        context.beginPath();
-        context.lineWidth = 1;
-        context.moveTo(centre.x, centre.y);
-        context.setLineDash([]);
-        context.lineTo(velocityVector.x, velocityVector.y);
-        context.stroke();
-
-        context.beginPath();
-        context.lineWidth = 1;
-        context.moveTo(centre.x, centre.y);
-        context.setLineDash([2, 1]);
-        context.lineTo(accelerationVector.x, accelerationVector.y);
-        context.stroke();
-
-        context.beginPath();
-        context.arc(centre.x, centre.y, size, 0, 2 * Math.PI, false);
-        context.fillStyle = color;
-        context.fill();
-        context.lineWidth = 1;
-        context.setLineDash([]);
-        context.strokeStyle = '#003300';
-        context.stroke();
+        //todo: generalise angle - not easy!
+        return {
+            scalar: Math.sqrt(sumOfSquares),
+            angle: Math.atan(inputVector[1] / inputVector[0])
+        };
     }
 
     return {
         init: function (paramScales) {
             scale = paramScales;
+
+            //var img = document.getElementById("background");
+
+            //context.drawImage(img, 0, 0);
+        },
+
+        clearScreen: function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        },
+
+        draw: function (position, velocity, acceleration, size, color) {
+            //console.log(color);
+
+            var centre = {
+                x: ((position[0] + scale / 2) / scale) * canvas.width,
+                y: canvas.height - ((position[1] + scale / 2) / scale) * canvas.height
+            }
+
+            var velocityVector = {
+                x: centre.x + velocity[0] * velocityScale,
+                y: centre.y - velocity[1] * velocityScale
+            }
+
+            var accelerationVector = {
+                x: centre.x + acceleration[0] * accelerationScale,
+                y: centre.y - acceleration[1] * accelerationScale
+            }
+
+            //draw velocity and the acceleration vectors
+            context.beginPath();
+            context.lineWidth = 1;
+            context.moveTo(centre.x, centre.y);
+            context.setLineDash([]);
+            context.lineTo(velocityVector.x, velocityVector.y);
+            context.stroke();
+
+            context.beginPath();
+            context.lineWidth = 1;
+            context.moveTo(centre.x, centre.y);
+            context.setLineDash([2, 1]);
+            context.lineTo(accelerationVector.x, accelerationVector.y);
+            context.stroke();
+
+            context.beginPath();
+            context.arc(centre.x, centre.y, size, 0, 2 * Math.PI, false);
+            context.fillStyle = color;
+            context.fill();
+            context.lineWidth = 1;
+            context.setLineDash([]);
+            context.strokeStyle = '#003300';
+            context.stroke();
         },
 
         updateText: function (nDims, momentumPolar, kineticEnergyPolar) {
             DOM.dimensions.innerHTML = nDims;
-            DOM.momentum.innerHTML = momentumPolar.scalar.toFixed(3) + ' @ ' + Math.round(momentumPolar.angle * 180 / Math.PI) + '&deg;';
-            DOM.kinetic.innerHTML = kineticEnergyPolar.scalar.toFixed(3) + ' @ ' + Math.round(kinetic.angle * 180 / Math.PI) + '&deg;';
+            DOM.momentum.innerHTML = formatLargeNumber(momentumPolar.scalar, 4) + ' @ ' + Math.round(momentumPolar.angle * 180 / Math.PI) + '&deg;';
+            DOM.kinetic.innerHTML = formatLargeNumber(kineticEnergyPolar.scalar, 4) + ' @ ' + Math.round(kineticEnergyPolar.angle * 180 / Math.PI) + '&deg;';
         },
 
-        drawThings: function (things) {
-            context.clearRect(0, 0, canvas.width, canvas.height);
+        addSomething: function (name, position, velocity, mass, size, color) {
+            /*
+            name, 
+            [0, orbit],
+            [speed, 0],
+            mass,
+            things[i].size,
+            things[i].color
+            */
 
-            things.forEach(function (thing) {
-                draw(thing);
-            })
+            var template = '<tr class="item" id="item-%name%">' +
+                '<td><div class="item__active">Yes <button class="btn-hold">No</button></div></td>' +
+                '<td><div class="item__name">%name%</div></td>' +
+                '<td><div class="item__mass">12345</div></td>' +
+                '<td><div class="item__orbit">12345</div></td>' +
+                '<td><div class="item__speed">12345</div></td>' +
+                '<td><div class="item__parent"></div></td>' +
+                '<td><div class="item__color">yellow</div></td>' +
+                '<td><div class="item__size">15</div></td>' +
+                '<td><div class="item__period">1 year</div></td>' +
+                '</tr>';
+
+            template = template.split('%name%').join(name);
+
+            //insert into DOM
+            DOM.item_list.insertAdjacentHTML('beforeend', template);
+        },
+
+        updateSomethingText: function (name, position, velocity, mass, size, color) {
+            /*
+            name, 
+            [0, orbit],
+            [speed, 0],
+            mass,
+            things[i].size,
+            things[i].color
+            */
+            var tr = document.getElementById('item-' + name);
+            tr.getElementsByClassName('item__orbit')[0].innerHTML = formatLargeNumber( toPolar(position).scalar, 4);
+            tr.getElementsByClassName('item__speed')[0].innerHTML = formatLargeNumber( toPolar(velocity).scalar, 4);
+            tr.getElementsByClassName('item__mass')[0].innerHTML = formatLargeNumber( mass,4);
+            tr.getElementsByClassName('item__size')[0].innerHTML = size;
+            tr.getElementsByClassName('item__color')[0].innerHTML = color;
         }
     }
 
 })();
 
-var controller = (function (uni, UICtrl) {
-    var earthOrbitalRadius = 146 * Math.pow(10, 9);
-    var moonOrbitalRadius = 384.4 * Math.pow(10, 6);
+//require( './things.json');
+var thingsConfig = [{
+        "name": "sun",
+        "mass": "1.9884700000:30",
+        "orbit": "0",
+        "speed": "0",
+        "parent": "",
+        "color": "yellow",
+        "size": "15"
+    },
+    {
+        "name": "earth",
+        "mass": "5.9720:24",
+        "orbit": "146:9",
+        "speed": "29.8:3",
+        "parent": "sun",
+        "color": "green",
+        "size": "5"
+    },
+    {
+        "name": "moon",
+        "mass": "7.34767309:22",
+        "orbit": "384.4:6",
+        "speed": "1.023:3",
+        "parent": "earth",
+        "color": "grey",
+        "size": "2"
+    }
+];
 
-    var scale = 3 * earthOrbitalRadius;
-    var centre = scale / 2;
-    var sunMass = 1.9884700000 * Math.pow(10, 30);
+
+var controller = (function (uni, UICtrl) {
+
+    var deFormat = function (raw) {
+        var split = raw.split(":");
+
+        var result = split[0] * (split[1] ? Math.pow(10, split[1]) : 1);
+
+        console.log(raw + ' --> ' + result);
+
+        return result;
+    }
 
     var populateTheUniverse = function () {
-        var moonMass = 7.34767309 * Math.pow(10, 22);
-        var earthMass = 5.9720 * Math.pow(10, 24);
-        var earthSpeed = 29.8 * Math.pow(10, 3);
-        var moonSpeed = 1.023 * Math.pow(10, 3);
+        var things = thingsConfig;
 
-        uni.createSomething([centre, centre], [0, 0], sunMass, 15, 'yellow');
-        uni.createSomething([centre, centre - earthOrbitalRadius], [earthSpeed, 0], earthMass, 5, 'green');
-        uni.createSomething([centre, centre - earthOrbitalRadius - moonOrbitalRadius], [earthSpeed + moonSpeed, 0], moonMass, 4, 'grey');
+        for (i = 0; i < things.length; i++) {
+            console.log("Creating {" + things[i].name + "}");
 
-        // for (j = 0; j < 5; j++) {
-        //     newRandomSomething();
-        // }
-    }
+            var mass = deFormat(things[i].mass);
+            var orbit = deFormat(things[i].orbit);
+            var speed = deFormat(things[i].speed);
+
+            var parent = things[i].parent;
+
+            while (parent) {
+                var found = false;
+
+                for (j = 0; j < things.length && !found; j++) {
+                    if (things[j].name === parent) {
+                        console.log("Parent is {" + things[j].name + "}");
+
+                        orbit += deFormat(things[j].orbit);
+                        speed += deFormat(things[j].speed);
+
+                        parent = things[j].parent;
+
+                        found = true;
+                    }
+                }
+            }
+
+            uni.createSomething(
+                things[i].name,
+                [0, orbit],
+                [speed, 0],
+                mass,
+                things[i].size,
+                things[i].color
+            );
+
+            UICtrl.addSomething(
+                things[i].name,
+                [0, orbit],
+                [speed, 0],
+                mass,
+                things[i].size,
+                things[i].color
+            );
+        }
+    };
+
+    var earthOrbitalRadius = 146 * Math.pow(10, 9);
+
+    var scale = 3 * earthOrbitalRadius;
 
     // How much are we speeded up?
     var timeFactor = Math.pow(10, 4);
 
-    var newRandomSomething = function () {
-        uni.createSomething([scale * Math.random(), scale * Math.random()],
-            [scale * Math.random() / (10 * timeFactor), scale * Math.random() / (10 * timeFactor)],
-            sunMass * Math.random() * 0.1,
-            Math.random() * 25,
-            'red');
-    }
-
     var clockTick = function () {
         for (t = 0; t < timeFactor; t++) {
             uni.moveAll();
+
+            //uni.wrapAll();
+
+            //uni.collide();
+
+            uni.applyGravity();
         }
 
-        uni.bounceAll();
+        UICtrl.clearScreen();
 
-        //uni.collide();
-
-        uni.applyGravity();
-
-        UICtrl.drawThings(uni.getThings());
-    }
+        uni.getThings().forEach(function (thing) {
+            UICtrl.draw(thing.position, thing.velocity, thing.acceleration, thing.size, thing.color);
+        });
+    };
 
     return {
         init: function () {
@@ -339,11 +480,15 @@ var controller = (function (uni, UICtrl) {
                 var metrics = uni.getMetrics();
 
                 UICtrl.updateText(nDims, metrics.momentum, metrics.kineticEnergy);
+
+                uni.getThings().forEach(function (thing) {
+                    UICtrl.updateSomethingText(thing.name, thing.position, thing.velocity, thing.mass, thing.size, thing.color);
+                });
             }, 750);
 
             console.log("Jump to hyperspace...");
         }
-    }
+    };
 })(universe, uiController);
 
 controller.init();
